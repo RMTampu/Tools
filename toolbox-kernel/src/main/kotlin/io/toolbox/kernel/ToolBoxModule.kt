cd76a8034ddb79f71af75d2f29166869cdf37e14
RMTampu <rikcyrish@gmail.com>
2026-08-30 17:15:56 +0700

    Add ToolBox module contract and kernel context

---
+++ toolbox-kernel/src/main/kotlin/io/toolbox/kernel/ToolBoxModule.kt
@@ -0,0 +1,21 @@
+package io.toolbox.kernel
+
+interface ToolBoxModule {
+    val descriptor: ModuleDescriptor
+
+    fun onLoad(context: KernelContext) = Unit
+
+    fun onStart() = Unit
+
+    fun onStop() = Unit
+
+    fun healthCheck(): HealthStatus = HealthStatus.ok()
+}
+
+data class KernelContext(
+    val config: KernelConfig,
+    val services: ServiceRegistry,
+    val capabilities: CapabilityRegistry,
+    val events: EventBus,
+    val commands: CommandBus
+)
