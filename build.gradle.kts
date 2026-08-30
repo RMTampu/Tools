966d7a707acc4d22da96733d100feb94eddf3464
RMTampu <rikcyrish@gmail.com>
2026-08-31 00:51:19 +0700

    Use lockfiles plus checksum verification for reproducible dependency resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,6 @@ allprojects {
     configurations.configureEach {
         resolutionStrategy {
             failOnVersionConflict()
-            failOnNonReproducibleResolution()
         }
     }
 }
