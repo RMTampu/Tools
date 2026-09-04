3f17d59f1ad9589e229098f8379973949258b781
RMTampu <rikcyrish@gmail.com>
2026-09-04 07:03:01 +0700

    Add Stage 1 application kernel foundation

---
+++ app/src/main/java/com/toolbox/tools/core/AppKernel.kt
@@ -0,0 +1,10 @@
+package com.toolbox.tools.core
+
+class AppKernel {
+    val toolRegistry = ToolRegistry()
+    val engineManager = EngineManager()
+
+    fun initialize() {
+        // Core bootstrap entry point
+    }
+}
