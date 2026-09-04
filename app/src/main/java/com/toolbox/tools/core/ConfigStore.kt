7111504467c0dfe5bf6d3b65333833c0871fa4f2
RMTampu <rikcyrish@gmail.com>
2026-09-04 08:05:49 +0700

    Harden ConfigStore with schema version foundation

--- app/src/main/java/com/toolbox/tools/core/ConfigStore.kt
+++ app/src/main/java/com/toolbox/tools/core/ConfigStore.kt
@@ -1,11 +1,20 @@
 package com.toolbox.tools.core
 
-class ConfigStore {
+class ConfigStore(private val schemaVersion: Int = 1) {
     private val values = mutableMapOf<String, String>()
 
+    fun version(): Int = schemaVersion
+
     fun put(key: String, value: String) {
+        require(key.isNotBlank())
         values[key] = value
     }
 
     fun get(key: String): String? = values[key]
+
+    fun contains(key: String): Boolean = values.containsKey(key)
+
+    fun clear() {
+        values.clear()
+    }
 }
