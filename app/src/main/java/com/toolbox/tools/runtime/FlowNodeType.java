b92d64b6d12f35e1b305ed2190c9ad5c720c2265
RMTampu <rikcyrish@gmail.com>
2026-09-04 12:29:11 +0700

    Add Tahap 4 flow node types

---
+++ app/src/main/java/com/toolbox/tools/runtime/FlowNodeType.java
@@ -0,0 +1,13 @@
+package com.toolbox.tools.runtime;
+
+public enum FlowNodeType {
+    EVENT,
+    ACTION,
+    CONDITION,
+    BRANCH,
+    ASYNC,
+    LOOP,
+    VARIABLE,
+    FUNCTION,
+    END
+}
