544cfe81c7b8d4036d0caa9c9f3098d72d1531c8
RMTampu <rikcyrish@gmail.com>
2026-09-04 10:43:23 +0700

    Add explicit Tahap 2 recovery publication to project store

--- app/src/main/java/com/toolbox/tools/core/ProjectStore.java
+++ app/src/main/java/com/toolbox/tools/core/ProjectStore.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 public interface ProjectStore {
     ProjectLoadResult load(String projectId) throws IOException;
+
     ProjectState commit(ProjectState workingState, long expectedRevision) throws IOException;
+
+    ProjectState recoverRevision(long revision) throws IOException;
+
     ProjectState loadRevision(long revision) throws IOException;
+
     List<RecoveryCandidate> recoveryCandidates() throws IOException;
 }
