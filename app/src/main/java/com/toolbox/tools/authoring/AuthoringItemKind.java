da4e72b1cd119c2536461367a0b97724c9bd3701
RMTampu <rikcyrish@gmail.com>
2026-09-04 14:02:16 +0700

    Add Tahap 6 unified searchable item kinds

---
+++ app/src/main/java/com/toolbox/tools/authoring/AuthoringItemKind.java
@@ -0,0 +1,13 @@
+package com.toolbox.tools.authoring;
+
+public enum AuthoringItemKind {
+    COMPONENT,
+    TEMPLATE,
+    ASSET,
+    SCREEN,
+    FLOW,
+    DATA_SOURCE,
+    BINDING,
+    ACTION,
+    EVENT
+}
