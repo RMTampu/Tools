486d181a009136473d9b8ae5b1590c3e60823dcf
RMTampu <rikcyrish@gmail.com>
2026-09-04 10:35:47 +0700

    Add Tahap 2 project access classification

---
+++ app/src/main/java/com/toolbox/tools/core/ProjectAccessStatus.java
@@ -0,0 +1,10 @@
+package com.toolbox.tools.core;
+
+public enum ProjectAccessStatus {
+    PROJECT_OK,
+    ACCESS_LOST,
+    FOLDER_MISSING,
+    RESOURCE_MISSING,
+    PROJECT_CORRUPT,
+    SCHEMA_INCOMPATIBLE
+}
