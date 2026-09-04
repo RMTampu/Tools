d926206eb3fb6185ba7e826bb848355bcfe3d4ee
RMTampu <rikcyrish@gmail.com>
2026-09-04 11:35:46 +0700

    Add Tahap 3 typed property kinds

---
+++ app/src/main/java/com/toolbox/tools/library/PropertyType.java
@@ -0,0 +1,15 @@
+package com.toolbox.tools.library;
+
+public enum PropertyType {
+    BOOLEAN,
+    NUMBER,
+    DIMENSION,
+    TEXT,
+    COLOR,
+    ASSET,
+    ENUM,
+    URI,
+    LIST,
+    OBJECT,
+    REFERENCE
+}
