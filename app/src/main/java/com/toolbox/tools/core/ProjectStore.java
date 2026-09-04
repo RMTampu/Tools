d86ef12f09d0f437f3b4621e380fc78cfbfa555c
RMTampu <rikcyrish@gmail.com>
2026-09-04 10:37:59 +0700

    Add Tahap 2 project store contract

---
+++ app/src/main/java/com/toolbox/tools/core/ProjectStore.java
@@ -0,0 +1,11 @@
+package com.toolbox.tools.core;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface ProjectStore {
+    ProjectLoadResult load(String projectId) throws IOException;
+    ProjectState commit(ProjectState workingState, long expectedRevision) throws IOException;
+    ProjectState loadRevision(long revision) throws IOException;
+    List<RecoveryCandidate> recoveryCandidates() throws IOException;
+}
