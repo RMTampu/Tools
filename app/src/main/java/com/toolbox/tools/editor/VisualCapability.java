bbd9ee7fa29f0026603ee3deda2d5572f1094d4e
RMTampu <rikcyrish@gmail.com>
2026-09-04 13:08:29 +0700

    Add Tahap 5 visual edit capability groups

---
+++ app/src/main/java/com/toolbox/tools/editor/VisualCapability.java
@@ -0,0 +1,24 @@
+package com.toolbox.tools.editor;
+
+public enum VisualCapability {
+    STYLE,
+    SIZE,
+    POSITION,
+    CONTENT,
+    COLOR,
+    SPACING,
+    SHAPE,
+    BORDER,
+    FONT_TEXT,
+    OPACITY,
+    TRANSFORM,
+    ALIGNMENT,
+    LAYER,
+    STATE,
+    ANIMATION,
+    AUTO_CONNECT_BINDING,
+    EVENT_ACTION,
+    ACCESSIBILITY,
+    LOCK,
+    OTHERS
+}
