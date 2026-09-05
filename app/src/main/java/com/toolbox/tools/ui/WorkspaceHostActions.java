e3f02aba1aafbc7c5b66c730a06fc7d85626c9c7
RMTampu <rikcyrish@gmail.com>
2026-09-05 11:49:06 +0700

    Tambahkan host aksi paket evolusi

--- app/src/main/java/com/toolbox/tools/ui/WorkspaceHostActions.java
+++ app/src/main/java/com/toolbox/tools/ui/WorkspaceHostActions.java
@@ -3,4 +3,6 @@ package com.toolbox.tools.ui;
 public interface WorkspaceHostActions {
     void requestExternalAsset();
     String externalAssetStatus();
+    void requestEvolutionPackage();
+    String evolutionPackageStatus();
 }
